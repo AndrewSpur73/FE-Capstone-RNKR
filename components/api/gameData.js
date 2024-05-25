2d402bc2d8190be66f7bd73e6a670fa720b204d1
Andrew <andrewspurlock@rocketmail.com>
2024-05-25 11:47:01 -0500

    gameData api calls made

---
+++ components/api/gameData.js
@@ -0,0 +1,73 @@
+import { clientCredentials } from '../../utils/client';
+
+const endpoint = clientCredentials.databaseURL;
+
+const getGames = (uid) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/game.json?orderBy="uid"&equalTo="${uid}"`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(Object.values(data)))
+    .catch(reject);
+});
+
+const deleteGames = (firebaseKey) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/game/${firebaseKey}.json`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve((data)))
+    .catch(reject);
+});
+
+const getSingleGame = (firebaseKey) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/game/${firebaseKey}.json`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+const createGame = (payload) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/game.json`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+const updateGame = (payload) => new Promise((resolve, reject) => {
+  fetch(`${endpoint}/game/${payload.firebaseKey}.json`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  })
+    .then((response) => response.json())
+    .then((data) => resolve(data))
+    .catch(reject);
+});
+
+export {
+  getGames,
+  createGame,
+  updateGame,
+  deleteGames,
+  getSingleGame,
+};
