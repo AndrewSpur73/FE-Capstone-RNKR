2d9e0d600b88d824c7c1d7d4a1fab384b63627a1
Andrew <andrewspurlock@rocketmail.com>
2024-05-28 18:52:23 -0500

    game card put on index page

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,34 @@
+import { useEffect, useState } from 'react';
+import { getGames } from '../components/api/gameData';
 import { useAuth } from '../utils/context/authContext';
+import GameRankCard from '../components/cards/GameRankCard';
 
 function Home() {
+  const [games, setGames] = useState([]);
   const { user } = useAuth();
 
+  const getAllGames = () => {
+    getGames(user.uid).then(setGames);
+  };
+
+  useEffect(() => {
+    getAllGames();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
   return (
-    <div
-      className="text-center d-flex flex-column justify-content-center align-content-center"
-      style={{
-        height: '90vh',
-        padding: '30px',
-        maxWidth: '400px',
-        margin: '0 auto',
-      }}
-    >
-      <h1>Hello {user.displayName}! </h1>
+    <div className="text-center my-4">
+      {/* <Link href="/member/new" passHref>
+        <Button>Add A Member</Button>
+      </Link> */}
+      <div className="d-flex flex-wrap">
+        {games.map((game) => (
+          <GameRankCard key={game.firebaseKey} gameObj={game} onUpdate={getAllGames} />
+        ))}
+      </div>
+
     </div>
   );
 }
 
 export default Home;
+
